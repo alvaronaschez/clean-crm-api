969765250cb2f08743cdc28436201c6f70d867de
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2023-11-24 14:52:11 +0100

    feat: add login and logout endpoints to openapi docs

--- src/main/java/com/github/alvaronaschez/crm/api/AuthController.java
+++ src/main/java/com/github/alvaronaschez/crm/api/AuthController.java
@@ -1,11 +1,19 @@
 package com.github.alvaronaschez.crm.api;
 
+import com.github.alvaronaschez.crm.api.dto.LoginRequestDTO;
 import com.github.alvaronaschez.crm.api.dto.UserOutDTO;
 import com.github.alvaronaschez.crm.configuration.security.SecurityUser;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
+
+import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,5 +27,16 @@ public class AuthController {
         var user = securityUser.getUser().toDomain();
 
         return UserOutDTO.fromDomain(user);
+    }
+
+    @PostMapping("/v1/login")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        throw new NotImplementedException("only for Openapi to be able to autogenerate docs");
+    }
+
+    @PostMapping("/v1/logout")
+    public void logout() {
+        throw new NotImplementedException("only for Openapi to be able to autogenerate docs");
     }
 }
